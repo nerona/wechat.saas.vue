f2ae7de673f6e4d7bf13769843c08bc70231224b
huojinzhao <352782712@qq.com>
2018-05-28 20:13:08 +0800

    refactor(Order): Detail

--- src/views/User/Order/Detail.vue
+++ src/views/User/Order/Detail.vue
@@ -10,14 +10,16 @@ import {
   XButton,
 } from 'vux';
 import { pageUtils } from '@/mixins';
-import DetailNote from './DetailNote';
+import OrderNote from './components/OrderNote';
+import OrderCard from './components/OrderCard';
 
 export default {
   name: 'UserOrderDetail',
 
   components: {
     Confirm,
-    DetailNote,
+    OrderNote,
+    OrderCard,
     XButton,
   },
 
@@ -86,8 +88,10 @@ export default {
         .then(this.onCancelAlert);
     },
 
-    toRefund() {
+    toRefund(route = this.$route) {
+      const id = Number(route.params.id);
 
+      this.$router.push(`/user/refund/${id}`);
     },
 
     toPay() {
@@ -102,50 +106,30 @@ export default {
     v-if="data"
     class="order-detail"
   >
-    <DetailNote :data="[statusText]"/>
-    <DetailNote :data="['学员姓名', data.student.name]"/>
-    <DetailNote :data="['订单类型', data.order_type_name]"/>
-
-    <div class="order-detail__info">
-      <div
-        :style="{
-          'background-image': `url(${data.course_packet.cover_url})`
-        }"
-        class="order-detail__avatar"
-      />
-      <ul class="order-detail__introduce">
-        <li>{{ data.course_packet.name }}</li>
-        <li>课程金额: <span>￥{{ data.amount_paid }}</span></li>
-        <li
-          v-if="data.is_can_refund"
-          class="order-detail__introduce__control"
-        >
-          <XButton
-            v-if="data.order_refund"
-            type="primary"
-            text="查看"
-            default
-            mini
-            @click.native="() => toCheck()"
-          />
-          <XButton
-            v-else
-            type="primary"
-            text="退款"
-            default
-            mini
-            @click.native="() => toRefund()"
-          />
-          <span>退费中</span>
-        </li>
-      </ul>
-    </div>
-
-    <DetailNote
-      v-if="Number(data.amount_pledge)"
-      :data="['押金抵扣',data.amount_pledge]"
+    <OrderNote :content="statusText"/>
+    <OrderNote
+      :content="data.student.name"
+      label="学员姓名"
     />
-    <DetailNote :data="['实付金额', data.amount_paid]"/>
+    <OrderNote
+      :content="data.order_type_name"
+      label="订单类型"
+    />
+
+    <OrderCard
+      :data="data"
+      @click="toRefund"
+    />
+
+    <OrderNote
+      v-if="Number(data.amount_pledge)"
+      :content="data.amount_pledge"
+      label="押金抵扣"
+    />
+    <OrderNote
+      :content="data.amount_paid"
+      label="实付金额"
+    />
 
     <ul class="order-detail__message">
       <li>订单编号: {{ data.order_number }}</li>
@@ -179,52 +163,6 @@ export default {
 @padding: 8px 14px;
 
 .order-detail button[default] {
-  background-color: @bg-default;
-}
-
-.order-detail__info {
-  display: flex;
-  margin: @margin;
-  padding: @padding;
-  background-color: white;
-}
-
-.order-detail__introduce {
-  list-style: none;
-  flex: auto;
-  min-width: 0;
-  font-size: 12px;
-}
-
-.order-detail__introduce > li:first-child {
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-}
-
-.order-detail__introduce > li:last-child > span {
-  color: @font-size-error-color;
-}
-
-.order-detail__introduce__control {
-  display: flex;
-  flex-direction: row-reverse;
-  justify-content: space-between;
-}
-
-.order-detail__introduce__control button {
-  margin: 0;
-  padding: 0;
-  width: 4em !important;
-  font-size: 12px;
-}
-
-.order-detail__avatar {
-  flex-shrink: 0;
-  background-size: 100% 100%;
-  margin-right: px2vw(@gutter-x);
-  height: px2vw(160);
-  width: px2vw(160);
   background-color: @bg-default;
 }
 
